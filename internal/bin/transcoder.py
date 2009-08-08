c28748e1d4789943e0b9cc5ca7a693e3fec420e7
Jonas Galvez <jonas@celine.(none)>
2009-08-08 19:28:51 -0300

    Transcoder updates: create all tmp dirs, save source URL and create symlinks to original file to facilitate job pass logic.

--- internal/bin/transcoder.py
+++ internal/bin/transcoder.py
@@ -21,19 +21,33 @@ class TranscoderDaemon(daemon.Daemon):
   default_conf = os.path.join(base, '..', 'config', 'transcoder.conf')
   section = 'transcoder' # which should conventionally be the same as the filename
   TMP_VIDEO_ROOT = '/mnt/tmp/publicvideos/transcoding_limbo'
+  if not os.path.exists(os.path.join(TMP_VIDEO_ROOT, 'originals')):
+    os.makedirs(os.path.exists(os.path.join(TMP_VIDEO_ROOT, 'originals')))
   S3_BUCKET_NAME = 'camera'
-  def save_tmp_video(self, job_slug, current_video):
+  def save_tmp_video(self, current_video):
     data = S3_CONN.get(TranscoderDaemon.S3_BUCKET_NAME, "originals/%s" % current_video.s3_key)
     original_filename = "%s.%s" % (current_video.s3_key, '0')
-    with open(os.path.join(S3UploaderDaemon.TMP_VIDEO_ROOT, job_slug, original_filename), 'wb') as f:
+    with open(os.path.join(S3UploaderDaemon.TMP_VIDEO_ROOT, 'originals', original_filename), 'wb') as f:
       f.write(data)
+    for job in self.jobs:
+      os.symlink(os.path.join(TranscoderDaemon.TMP_VIDEO_ROOT, 'originals', original_filename),
+                 os.path.join(TranscoderDaemon.TMP_VIDEO_ROOT, job.job_slug, original_filename))
   def put_the_result_back_in_s3(self, job_slug, current_video, result):
     options = {'Content-Type': current_video.mimetype or 'application/octet-stream', 'X-Amz-Acl': 'private'}
     with open(result, 'rb') as f:
       result = f.read()
     transcoded_file = S3.S3Object(result)
     S3_CONN.put(TranscoderDaemon.S3_BUCKET_NAME, "%s/%s" % (job_slug, current_video.s3_key), transcoded_file, options)
+    return S3_URL_GENERATOR.generate_url('GET', TranscoderDaemon.S3_BUCKET_NAME, "%s/%s" % (job_slug, current_video.s3_key))  
+  def load_jobs(self):
+    self.jobs = models.TranscodingJob.objects.all()
+    for job in self.jobs:
+      if not os.path.exists(os.path.join(TranscoderDaemon.TMP_VIDEO_ROOT, job.job_slug)):
+        os.makedirs(job.job_slug)
   def run(self):
+    if not os.path.exists(TranscoderDaemon.TMP_VIDEO_ROOT):
+      os.makedirs(TranscoderDaemon.TMP_VIDEO_ROOT)
+    self.load_jobs()
     while True:
       try:
         cursor = models.conn.cursor()
@@ -45,8 +59,7 @@ class TranscoderDaemon(daemon.Daemon):
           time.sleep(30)
           continue
         logging.info("Downloading original video %s so we can transcode the shit out of it." % current_video.s3_key)
-        self.save_tmp_video(current_video.s3_key)
-        jobs = models.TranscodingJob.objects.all()
+        self.save_tmp_video_and_create_references(current_video)        
         current_video.status = 'transcoding'
         current_video.save()
         utils.unlock_and_lock_again_real_quick(cursor, 'video_queue')
@@ -59,8 +72,8 @@ class TranscoderDaemon(daemon.Daemon):
             target_pass_path = os.path.join(TranscoderDaemon.TMP_VIDEO_ROOT, job_slug, target_pass_filename)
             command = job_pass.command.replace('$SOURCE', source_pass_path).replace('$TARGET', target_pass_path)
             command_status, command_output = commands.getstatusoutput(command)
-          self.put_the_result_back_in_s3(current_video, job.job_slug, target_path_pass)
-          VideoVersion(video=current_video, trancoded_with=job, source='') # todo: generate signed source link
+          source_url = self.put_the_result_back_in_s3(current_video, job.job_slug, target_path_pass)
+          VideoVersion(video=current_video, trancoded_with=job, url=source_url)
           logging.info("Transcoded and uploaded video %s with the %s encoding." % (current_video.s3_key, job.job_slug))          
         utils.unlock_on_string(cursor, 'video_queue');
       except:
@@ -74,5 +87,8 @@ class TranscoderDaemon(daemon.Daemon):
 
 if __name__ == '__main__':
   AWS_CREDENTIALS = utils.load_aws_credentials()
-  S3_CONN = S3.AWSAuthConnection(AWS_CREDENTIALS.S3.access_key, AWS_CREDENTIALS.S3.secret_key)
+  ac, sk = AWS_CREDENTIALS.S3.access_key, AWS_CREDENTIALS.S3.secret_key
+  S3_CONN = S3.AWSAuthConnection(ac, sk)
+  S3_URL_GENERATOR = S3.QueryStringAuthGenerator(ac, sk, is_secure=False)
+  S3_URL_GENERATOR.set_expires_in(60*60*2)
   TranscoderDaemon().main()
