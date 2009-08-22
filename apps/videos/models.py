dfca45810cc5d34dd6ca7d56bb63662723e4bbe7
Fabricio Zuardi <fabricio@fabricio.org>
2009-08-22 19:45:10 -0300

    some documentation for the video models

--- apps/videos/models.py
+++ apps/videos/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 
 class Video(models.Model):
+  """A video clip instance. The metadata about the clip and it's
+  original uploaded file.
+  """
   STATUS_CHOICES = (
     ('pending_upload_to_s3', 'Upload pending'),
     ('pending_transcoding', 'Uploaded, transcoding pending'),
@@ -27,21 +30,35 @@ class Video(models.Model):
     return self.title
 
 class TranscodingJob(models.Model):
+  """The object representing a video transcoding process, the conversion
+  of a video file of one format into another.
+  Example: 60fps 1920x1080 mts to 30fps 640x360 ogg.
+  """
   description = models.CharField(max_length=100, blank=True, null=True)
   job_slug = models.CharField(max_length=15, blank=True, null=True)
 
 class TranscodingPass(models.Model):
+  """The object representing an individual step (encoding pass) in a 
+  transcoding job, usually this means one call to a video converter tool.
+  Different TranscodingJobs may share some TranscodingPass steps, for 
+  example: the convertion from an original camera file to an 
+  intermediate format can be the first step of different TranscodingJobs
+  """
   from_extension = models.CharField(max_length=10, blank=True, null=True)
   to_extension = models.CharField(max_length=10, blank=True, null=True)
   description = models.CharField(max_length=100, blank=True, null=True)
   command = models.TextField()
 
 class TranscodingJobPass(models.Model):
+  """The relationship table for transcoding steps and transcoding jobs.
+  """
   transcoding_job = models.ForeignKey(TranscodingJob, blank=True, null=True)
   transcoding_pass = models.ForeignKey(TranscodingPass, blank=True, null=True)
   step_number = models.PositiveIntegerField(blank=True, null=True)
 
 class VideoVersion(models.Model):
+  """A video derivate, the files that are actually used by the website.
+  """
   source = models.ForeignKey(Video, blank=True, null=True)
   url = models.URLField(verify_exists=False, max_length=400, blank=True, null=True)
   mimetype = models.CharField(max_length=40, blank=True, null=True)
