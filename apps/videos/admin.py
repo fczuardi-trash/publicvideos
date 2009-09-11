e98da1baab346861b79bc2cc75799feb8e5353ac
Fabricio Zuardi <fabricio@fabricio.org>
2009-09-11 15:03:41 -0300

    tweaking on the listing display for Video objects on the admin page

--- apps/videos/admin.py
+++ apps/videos/admin.py
@@ -1,7 +1,10 @@
 from videos.models import Video, TranscodingJob, TranscodingPass, TranscodingJobPass, VideoVersion
 from django.contrib import admin
 
-admin.site.register(Video)
+class VideoAdmin(admin.ModelAdmin):
+  list_display = ('filename', 'md5', 'status', 'set_slug')
+
+admin.site.register(Video, VideoAdmin)
 admin.site.register(TranscodingJob)
 admin.site.register(TranscodingPass)
 admin.site.register(TranscodingJobPass)
