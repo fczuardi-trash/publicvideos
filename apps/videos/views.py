019d701d74290efc45294abb34b3d0cdb80e0739
Fabricio Zuardi <fabricio@Fabricio-Zuardis-MacBook-Pro.local>
2010-01-04 20:40:50 -0200

    a simple clips index page

--- apps/videos/views.py
+++ apps/videos/views.py
@@ -30,8 +30,18 @@ from django.contrib.auth.models import User
 
 
 def index(request):
-  videos = Video.objects.filter(status='transcoded')
-  videos_length = len(videos)
+  try:
+    videos = Video.objects.filter(status='transcoded').order_by('?')
+  except IndexError:
+    raise Http404
+  # http://www.archive.org/download/ace_200910_02/a391d9f8b519255ef21e36c1a75265a5.mts-jpg-108.JPG
+  # http://www.archive.org/download/ace_200910_01/025d08c0ab447e69dbccbb99a1df0b35.mts-jpg-108.JPG
+  # http://www.archive.org/download/ace_200907_01/33470ecf16669eb165619a9e229ce751.mts-jpg-108.JPG.JPG
+  thumbs = []
+  for video in videos[:47]:
+    url = "http://www.archive.org/download/%s/%s.%s" % (video.set_slug, video.md5, 'mts-jpg-108.JPG')
+    page = "/clip/?h=%s" % video.md5
+    thumbs.append({'src':url,'page':page})
   return render_to_response("videos/index.html", locals())
 
 def show(request):
