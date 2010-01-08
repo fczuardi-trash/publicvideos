dbfe37fe2dcad079f364b52408c30b8f0b42ce49
Fabricio Zuardi <fabricio@Fabricio-Zuardis-MacBook-Pro.local>
2010-01-08 14:57:51 -0200

    about page

--- urls.py
+++ urls.py
@@ -2,13 +2,15 @@ from django.conf import settings
 from django.conf.urls.defaults import *
 from django.contrib import admin
 from django.contrib.auth.views import login, logout
-#from lib.jinjasupport import jenv
+from lib.jinjasupport import jenv
 #from videos.models import Video
 
 admin.autodiscover()
 
+
 urlpatterns = patterns('',
   url(r'^$', 'videos.views.index', name="site_index_url"),
+  (r'^about/?$', 'django.views.generic.simple.direct_to_template', {'template': 'website/about.html','template_loader':jenv}),
   (r'^account/', include('django_authopenid.urls')),
   (r'^clip', include('videos.urls')),
   (r'^users', include('users.urls')),
@@ -19,15 +21,18 @@ urlpatterns = patterns('',
 
 # external links that appears on the beta-coming soon page
 urlpatterns += patterns('django.views.generic.simple',
-  url(r'^development/$', 'redirect_to', 
+  url(r'^development/?$', 'redirect_to', 
     {'url': 'https://launchpad.net/publicvideos'}, 
     name="development_url"),
-  url(r'^launch_notification/$', 'redirect_to', 
+  url(r'^launch_notification/?$', 'redirect_to', 
     {'url': 'http://spreadsheets.google.com/viewform?formkey=dEFNRU96SXBkWFdjc2pWeXBZNE5KRHc6MA..'}, 
     name="launch_notification_form_url"),
-  url(r'^join_beta/$', 'redirect_to', 
+  url(r'^join_beta/?$', 'redirect_to', 
     {'url': 'http://spreadsheets.google.com/viewform?formkey=dHJPZzczbVhuR0tnX0doaFRJVE5IX1E6MA..'}, 
     name="join_beta_form"),
+  url(r'^faq/?$', 'redirect_to', 
+    {'url': 'http://www.formspring.me/publicvideos'}, 
+    name="faq_url"),
 )
 
 urlpatterns += patterns('',
