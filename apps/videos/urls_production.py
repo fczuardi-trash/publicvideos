258e6a06de85c3e3f392383f3429886c4fbc75c9
Fabricio Zuardi <fabricio@Fabricio-Zuardis-MacBook-Pro.local>
2010-01-08 12:46:11 -0200

    forgot a file on last commit

--- apps/videos/urls_production.py
+++ apps/videos/urls_production.py
@@ -2,5 +2,6 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
     (r'^/?$', 'videos.views.show'),
+    (r'^/(?P<id>.+)/?$', 'videos.views.show'),
     (r'^s/?$', 'videos.views.index'),
 )
