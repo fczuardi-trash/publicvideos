a79d038f92cf4af44eb16794e1de61c31ea395d9
Fabricio Zuardi <fabricio@Fabricio-Zuardis-MacBook-Pro.local>
2010-01-08 23:46:07 -0200

    104 new clips

--- bin/create_archive_files_xml.py
+++ bin/create_archive_files_xml.py
@@ -2,9 +2,9 @@
 
 import os
 
-IDENTIFIER = 'ace_200911_03'
-VIDEOS_PATH = '/tmp/publicvideos/versions/ace_200911_03/'
-VIDEOS_TITLE = "Publicvideos November 2009 batch, part 1"
+IDENTIFIER = 'ace_200911_04'
+VIDEOS_PATH = '/tmp/publicvideos/versions/ace_200911_04/'
+VIDEOS_TITLE = "Publicvideos November 2009 batch, part 4"
 VIDEOS_DESCRIPTION = 'A batch of stock clips made for publicvideos.org.'
 VIDEOS_LABELS = 'publicvideos.org; royalty free; free; cc0; cczero; stock footage; ace of spades'
 VIDEOS_CREATOR = 'Ace of Spades'
