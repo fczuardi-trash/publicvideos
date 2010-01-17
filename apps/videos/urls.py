d4da747582952d444f0a474c1b32161ca27109fe
Fabricio Zuardi <fabricio@Fabricio-Zuardis-MacBook-Pro.local>
2010-01-17 01:14:43 -0200

    minor fix on the clip urls

--- apps/videos/urls.py
+++ apps/videos/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
-    (r'^/?$', 'videos.views.show'),
-    (r'^/(?P<id>.+)/?$', 'videos.views.show'),
+    (r'^\/?$', 'videos.views.show'),
+    (r'^/(?P<id>[^\/]+)\/?$', 'videos.views.show'),
     url(r'^s/?$', 'videos.views.index', name="clips_index_url"),
     (r'^s/upload/?$', 'videos.views.upload_videos'),
     (r'^s/simple_upload/?$', 'videos.views.simple_upload_videos'),
--- apps/videos/urls_production.py
+++ apps/videos/urls_production.py
@@ -1,7 +1,7 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
-    (r'^/?$', 'videos.views.show'),
-    (r'^/(?P<id>.+)/?$', 'videos.views.show'),
+    (r'^\/?$', 'videos.views.show'),
+    (r'^/(?P<id>[^\/]+)\/?$', 'videos.views.show'),
     (r'^s/?$', 'videos.views.index'),
 )
