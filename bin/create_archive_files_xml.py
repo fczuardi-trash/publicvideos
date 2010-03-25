7932635022e7e9041a14c1472c9f0070241511bf
Fabricio Zuardi <fabricio@187-27-206-66.3g.claro.net.br>
2010-03-25 00:37:35 -0300

    11 new clips

--- bin/create_archive_files_xml.py
+++ bin/create_archive_files_xml.py
@@ -2,13 +2,13 @@
 
 import os
 
-IDENTIFIER = 'taboca_201003_02'
-VIDEOS_PATH = '/tmp/publicvideos/versions/taboca_201003_02/'
-VIDEOS_TITLE = "Publicvideos March 2010 (Marcio) batch, part 2"
+IDENTIFIER = 'fabricio_201003_01'
+VIDEOS_PATH = '/tmp/publicvideos/versions/fabricio_201003_01/'
+VIDEOS_TITLE = "Publicvideos March 2010 (Fabricio) batch, part 1"
 VIDEOS_DESCRIPTION = 'A batch of stock clips made for publicvideos.org.'
 VIDEOS_LABELS = 'publicvideos.org; royalty free; free; cc0; cczero; stock footage; ace of spades'
-VIDEOS_CREATOR = 'Marcio Galli'
-VIDEOS_DIRECTOR = 'Marcio Galli'
+VIDEOS_CREATOR = 'Fabricio Zuardi'
+VIDEOS_DIRECTOR = 'Fabricio Zuardi'
 VIDEOS_YEAR = '2009'
 VIDEOS_UPLOADER = 'fabricio@fabricio.org'
 VIDEOS_SPONSOR = 'Fabricio Zuardi'
